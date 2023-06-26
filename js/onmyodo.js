fedc0e86bfe50922f58d3715e0122c5f08ea8b14
Mark <markmargulis@proton.me>
2023-06-26 16:06:33 -0400

    move selector mechanics are created

--- js/onmyodo.js
+++ js/onmyodo.js
@@ -1,9 +1,64 @@
 //variables
+
+//picked element
 let elm;
 
+//curently active button
+let activeButton;
+
+//random number for computer picked element
+
+let randElementPick = Math.random();
+//computer move
+
+let computerMove;
+
+
+
+
+
 //functions
+
+//element picker player
 function elmPicker(elem) {
   elm = elem.innerHTML;
+  removeActiveClass();
+  elem.classList.add('active');
+  console.log(elm);
+}
+
+//active class removal
+function removeActiveClass() {
+  activeButton = document.querySelector('.active');
+  if (activeButton) {
+    activeButton.classList.remove('active');
+  }
+}
+
+//element selector computer
+function computerMoveGen (){
   
- elem.classList.add('active');
+   if (randElementPick >= 0  && randElementPick < 1/5) {
+    computerMove = "Tree";
+   }
+   else if (randElementPick >= 1/5  && randElementPick < 2/5){
+    computerMove = "Fire";
+   }
+  
+   else if (randElementPick >= 2/5  && randElementPick < 3/5){
+    computerMove = "Earth";
+   }
+   else if (randElementPick >= 3/5  && randElementPick < 4/5){
+    computerMove = "Metal";
+   }
+   else if (randElementPick >= 4/5  && randElementPick < 1){
+    computerMove = "Water";
+   }
+   console.log(randElementPick);
+   console.log(computerMove);
+   randElementPick = Math.random();
+}
+
+function test(){
+  console.log(randElementPick);
 }
